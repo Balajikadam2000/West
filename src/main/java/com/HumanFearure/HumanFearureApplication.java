1989f959165ced42559b6c82bb1fa7a5c403e3c2
Balaji Kadam <balajikadam@gmail.com>
2025-12-24 19:54:59 +0530

    Somthing Added

---
+++ src/main/java/com/HumanFearure/HumanFearureApplication.java
@@ -0,0 +1,17 @@
+package com.HumanFearure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanFearureApplication {
+
+    int x = 39;
+    int y = 44;
+    
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanFearureApplication.class, args);
+	}
+
+}
